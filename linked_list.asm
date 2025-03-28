0680495b3a4163bd60fa57ed070ab508e81a63ff
Petroncini <caiopetroncini@gmail.com>
2025-03-28 20:28:39 -0300

    working linked list, structured as functions

--- linked_list.asm
+++ linked_list.asm
@@ -1,21 +1,26 @@
 	.data
 	.align 0
-str3:	.asciz "Operação (a, r): "
+str0:	.asciz "\nOperação (a, r): "
+str1: 	.asciz "\nOperando: "
+str2: 	.asciz "\nNo removido: "
 str4:	.asciz "Resultado: "
 ln:	.asciz "\n"
-base:	.space 8
+	.align 2
+base:	.word 4
 	.text
 	.align 2
 	.globl main
 	
 main:
 	la t0, base
-	sw zero, 0(t0)
-	sw zero, 4(t0) # zero base adress
+	sw zero, 0(t0) # zero base->node
 
 main_loop:
-	la a0, str3
+	la a0, str0 # printar prompt
 	li a7, 4
+	ecall
+	
+	li a7, 12
 	ecall
 	
 	li t0, 'a'
@@ -24,27 +29,77 @@ main_loop:
 	li t0, 'r'
 	beq a0, t0, remove_node
 	
-
 add_node:
+	la a0, str1
+	li a7, 4
+	ecall
+	
 	li a7, 5 # get number from user
 	ecall
 	
+	jal ra, func_add_node
+	j main_loop
+
+remove_node:
+	jal ra, func_remove_node
+	
+	la a0, str2 # printa str2
+	li a7, 4
+	ecall
+	
+	mv a0, a1 # printa valor do no removido
+	li a7, 1
+	ecall
+	
+	j main_loop
+	
+
+# funcao adicionar nó
+# a0 contém valor a ser adicionado
+# retorna nada
+func_add_node:
 	mv s0, a0 # number to be added
 	
 	li a7, 9 # alocate node space in heap
 	li a0, 8
 	ecall
 	
-	sw s0, 0(a0) # save value to node.num
+	# a0 agora contém endereço de novono
 	
-	la t0, base
-	lw t0, 0(t0) # load base address
+	sw s0, 0(a0) # salva valor em novono.num
 	
-	sw t0, 4(a0) # save base adress to node.next
+	la t0, base # t0 recebe endereço da base
+	lw t1, 0(t0) # t1 receve conteudo da base (endereço do primeiro nó)
 	
-	sw a0, 0(t0) # change base address to point to new node
+	sw t1, 4(a0) # salva endereço do primeiro nó em novono.next
 	
-	j main_loop
+	sw a0, 0(t0) # salva endereço de novono na base
+	
+	jr ra
+	
+# funcao remover nó
+# recebe nada
+# retorna valor removido em a1
+func_remove_node:
+	la t0, base # t0 recebe endereço para base
+	lw t1, 0(t0) # t1 recebe conteudo da base (endereço do primeiro nó)
+	
+	beq t1, zero, main_loop # se o conteúdo da base for 0, lista está vazia
+	
+	
+	lw t2, 4(t1) # t2 recebe endereço do segundo nó (base->node->next)
+	lw a1, 0(t1) # t1 recebe conteúdo do primeiro no
+	
+	sw t2, 0(t0) # base recebe endereço do segundo nó (base->node->next)
+	
+	jr ra
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
